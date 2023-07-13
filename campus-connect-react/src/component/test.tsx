a04d77af88e534622da398c392e9f0c6a33efccb
ShadowSkeleton <fengj5@rpi.edu>
2023-07-12 20:40:01 -0400

    test

---
+++ campus-connect-react/src/component/test.tsx
@@ -0,0 +1,14 @@
+// function write a hello world test with h
+
+function Test() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-blue-500">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default Test
