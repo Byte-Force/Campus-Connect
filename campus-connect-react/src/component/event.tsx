1db52b842c82bc44ea70f14600540c9f2e23164f
RC <91489366+RonglinC@users.noreply.github.com>
2023-07-24 17:47:58 -0400

    Update event.tsx

--- campus-connect-react/src/component/event.tsx
+++ campus-connect-react/src/component/event.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
 
 interface Event {
-  title: string;
-  date: string;
+  event: string;
+  start_date: string;
   location: string;
-  time: string;
+  // time: string;
 }
 
 const EventBoard: React.FC = () => {
@@ -15,9 +15,9 @@ const EventBoard: React.FC = () => {
       try {
         const response = await fetch('http://localhost:3000/db/events');
         const data = await response.json();
-        const sortedEvents = data.events.sort((a, b) => {
-          const Timea = a.start_date;
-          const Timeb = b.start_date;
+        const sortedEvents = data.events.sort((a: Event, b: Event) => {
+          const Timea = a.start_date; // 'date' property instead of 'start_date'
+          const Timeb = b.start_date; // 'date' property instead of 'start_date'
           return Timea < Timeb ? -1 : Timea > Timeb ? 1 : 0;
         });
         setEvents(sortedEvents);
@@ -31,9 +31,9 @@ const EventBoard: React.FC = () => {
   return (
     <div className="border-white m-1 p-0.5 bg-gray-200 rounded-sm">
       <h1 className="text-2xl font-bold mb-4">Upcoming Events</h1>
-      <div className="grid grid-cols-1 gap-2">
+      <div className="rounded-sm grid grid-flow-row grid grid-cols-2 gap-3">
         {events.map((event, index) => (
-          <div key={index} className="bg-pink-100 p-4 rounded-sm grid grid-cols-2 ">
+          <div key={index} className="bg-pink-100 p-4 rounded-sm grid grid-flow-row grid grid-cols-1 ">
             {/* Event date */}
             <div>
               <p className="text-black font-semibold text-center">{event.start_date}</p>
