b108d4f9f19e9a2b3750d8c6810d4818d0d9aced
yangw3 <91489504+yangw3@users.noreply.github.com>
2023-08-13 15:26:08 -0400

    update

---
+++ campus-connect-react/src/pages/ContactUsPage.tsx
@@ -0,0 +1,28 @@
+import Headers from '../component/header';
+
+
+import ContactUs from '../component/contactUS';
+import Footer from '../component/footer';
+
+
+
+export default function ContactUsPage() {
+
+    return (
+        <div>
+            <Headers />
+
+
+
+
+            {/* CategoryPosts */}
+
+            <ContactUs />
+
+
+
+
+            <Footer />
+        </div>
+    );
+}
